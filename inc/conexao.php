ec91f75f7028636f08bc2e1670407fd40e8f6b47
tonhus <antonio_abrantes@hotmail.com.br>
2017-10-28 13:07:21 -0300

    commit v7b data mysql2

--- inc/conexao.php
+++ inc/conexao.php
@@ -2,6 +2,7 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
+
 $hostname_conexao = "us-cdbr-iron-east-05.cleardb.net";
 $database_conexao = "heroku_b3a6eeed51132eb";
 $username_conexao = "bc4f6c49b48f09";
